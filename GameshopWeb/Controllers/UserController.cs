f600448eee88616906746913635fc3cba17ae83c
tvbegovic <tvrtko@adamant.hr>
2026-01-23 20:18:34 +0100

    login i token

--- GameshopWeb/Controllers/UserController.cs
+++ GameshopWeb/Controllers/UserController.cs
@@ -1,7 +1,12 @@
 ﻿using Dapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace GameshopWeb.Controllers
 {
@@ -10,9 +15,12 @@ namespace GameshopWeb.Controllers
 	public class UserController : ControllerBase
 	{
 		private IConfiguration configuration;
-		public UserController(IConfiguration configuration)
+		private JWTTokenConfig _jwtTokenConfig;
+		public UserController(IConfiguration configuration, 
+			JWTTokenConfig jwtTokenConfig)
 		{
 			this.configuration = configuration;
+			_jwtTokenConfig = jwtTokenConfig;
 		}
 
 		[HttpPost("register")]
@@ -41,5 +49,71 @@ VALUES(@firstname, @lastname,@email, @address, @City,@password)";
 			}
 
 		}
+
+		[HttpGet("login")]
+		public IActionResult Login(string email, string password)
+		{
+			using (SqlConnection connection = new SqlConnection(configuration.GetConnectionString("connString")))
+			{
+				string sql = "SELECT * FROM [User] WHERE email = @email AND password = @password";
+				User user = connection.QueryFirstOrDefault<User>(sql, new { email, password });
+				if (user == null)
+				{
+					return BadRequest("Ne postoji korisnik s tom email adresom i lozinkom");
+				}
+				LoginResult loginResult = new LoginResult();
+				user.Password = null;
+				loginResult.User = user;
+				loginResult.AccessToken = GenerateToken(email, "user");
+				return Ok(loginResult);
+			}
+		}
+
+		[HttpPut("")]
+		[Authorize]
+		public IActionResult UpdateUser(User user)
+		{
+			if(string.IsNullOrEmpty(user.Firstname) || string.IsNullOrEmpty(user.Lastname))
+			{
+				return BadRequest("Ime i prezime moraju biti upisani");
+			}
+			string sql = "UPDATE [User] SET firstname = @firstname, lastname = @lastname, address = @address, city = @city WHERE id = @id";
+			using (SqlConnection connection = new SqlConnection(configuration.GetConnectionString("connString")))
+			{
+				connection.Execute(sql, user);
+				return Ok();
+			}
+		}
+
+		private string GenerateToken(string email, string role)
+		{
+			var tokenHandler = new JwtSecurityTokenHandler();
+			var keyBytes = Encoding.UTF8.GetBytes(_jwtTokenConfig.Secret);
+
+			var tokenDescriptor = new SecurityTokenDescriptor
+			{
+				Subject = new ClaimsIdentity(new[]
+					{
+						new Claim(ClaimTypes.Name, email),
+						new Claim(ClaimTypes.Role, role)
+				}),
+				Expires = DateTime.UtcNow.AddMinutes(_jwtTokenConfig.AccessTokenExpiration),
+				Issuer = _jwtTokenConfig.Issuer,
+				Audience = _jwtTokenConfig.Audience,
+				SigningCredentials = new SigningCredentials(
+							new SymmetricSecurityKey(keyBytes),
+							SecurityAlgorithms.HmacSha256Signature
+					)
+			};
+
+			var token = tokenHandler.CreateToken(tokenDescriptor);
+			return tokenHandler.WriteToken(token);
+		}
+	}
+
+	public class LoginResult
+	{
+		public User User { get; set; }
+		public string AccessToken { get; set; }
 	}
 }
--- GameshopWeb/DomainClasses.cs
+++ GameshopWeb/DomainClasses.cs
@@ -31,11 +31,11 @@
 		public int Id { get; set; }
 		public string Firstname { get; set; }
 		public string Lastname { get; set; }
-		public string Address { get; set; }
+		public string? Address { get; set; }
 		public string Email { get; set; }
-		public string Password { get; set; }
+		public string? Password { get; set; }
 		public bool? Admin { get; set; }
-		public string City { get; set; }
+		public string? City { get; set; }
 	}
 	public class Game
 	{
